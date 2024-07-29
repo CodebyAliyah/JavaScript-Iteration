41083ae8218f1cb74f7f5770bcfe755c8aa5227c
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-07-29 09:49:54 -0700

    Code Done

---
+++ lteration.js
@@ -0,0 +1,45 @@
+
+// for loop : 
+
+// for (i = 0 ; i<=10 ; i++) {
+//   for (let j = 0 ; j<=10 ; j++) {
+//     console.log( i + '*' +  j + '=' + i*j)
+//   }
+    
+    
+//   }
+
+// for (i=0; i<10 ; i++) {
+//   if(i%2==0)
+//   console.log(i)
+// }
+
+
+// **** break **** //
+
+
+// for (let index = 1; index <= 15 ; index++) {
+//   if(index==5) {
+//     console.log( `detected : ${index}`)
+//     break ;
+//   }
+//   console.log(index)
+// }
+
+// ***** Continue **** //
+
+
+for (let index = 1; index <= 15 ; index++) {
+  if(index==5) {
+    console.log( `detected : ${index}`)
+    continue ;
+  }
+  console.log("value of i is" + index)
+}
+
+
+
+
+
+
+
